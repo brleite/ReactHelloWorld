0615759dea1b41104c3ed341fffd57417248a0b0
Bruno Mayerle Leite <brleite@gmail.com>
2020-10-20 09:18:31 -0300

    Ajustes iniciais

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, StatusBar } from "react-native";
 
+import api from "./services/api";
+
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    console.log("Create");
+    api
+      .get("/projects")
+      .then((response) => {
+        console.log("Get Projects");
+        console.log(response.data);
+        setProjects(response.data);
+      })
+      .catch((e) => {
+        console.log("Error");
+        console.log(e);
+      });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159C1" translucent />
