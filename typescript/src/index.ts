a3fe00b8f40dd7eef1460a88f76421755e5e0aaa
Bruno Mayerle Leite <brleite@gmail.com>
2020-10-20 10:53:03 -0300

    Mais de typescript

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,9 +1,12 @@
 import express, { response } from "express";
+import { helloWorld } from "./routes";
 
 const app = express();
 
 app.get("/", (request, response) => {
   return response.json({message:"Hello World"});
-})
+});
+
+app.get("/helloWorld", helloWorld);
 
 app.listen(19001);
