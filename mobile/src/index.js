d75b27a8794409ebcdfdce473a6c219a3e37ac89
Bruno Mayerle Leite <brleite@gmail.com>
2020-10-20 08:39:30 -0300

    Criação do projeto

---
+++ mobile/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159C1" translucent />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello World React Native</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159C1",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+});
